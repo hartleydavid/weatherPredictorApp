5ed3b891a3d4d6cd47e0ed2483e2ff9bf41ac662
hartleydavid <56169730+hartleydavid@users.noreply.github.com>
2024-03-30 05:47:42 -0700

    Calendar correctly shows invalid dates

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -6,16 +6,28 @@ import Calendar from 'react-calendar';
 function App() {
 
   const todaysDate = new Date();
+  const minDate = new Date(todaysDate.getFullYear(), todaysDate.getMonth(), todaysDate.getDate()-1);
+
+  const tileClassName = ({ date }) => {
+    // Check if the date is before minDate
+    if (date <= minDate) {
+      //return 'before-min-date';
+      return 'before-today';
+    }
+    return null;
+  };
 
   return (
     <div className="App">
       <header>  Weather Prediction App </header>
-
+      <h2> Please select a valid date. </h2>
+      <p> Valid dates are from todays date and on.</p>
     {/*Create calendar that only allows selections of todays date and forward, no past dates*/}
-
     <Calendar 
-      minDate={new Date(todaysDate.getFullYear(), todaysDate.getMonth(), todaysDate.getDate())}
+      minDate={todaysDate}
+      minDetail='Month'
       calendarType='US'
+      tileClassName={tileClassName}
     />
 
     </div>
